006880a7b182bb9ea1c10d8d0e1f2817cf60ebe2
Jiranans <baimonjiranan@gmail.com>
2023-03-31 21:34:21 +0700

    commit

---
+++ main/java/myexception2/Exception1.java
@@ -0,0 +1,8 @@
+
+package myexception2;
+import java.util.Scanner;
+public class Exception1 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ main/java/myexception2/lab1.java
@@ -0,0 +1,18 @@
+
+package myexception2;
+
+
+public class lab1 {
+     public static void main(String[] args) {
+        System.out.println(1/0);
+       int[] list = new int[5];
+       System.out.println(list[4]);
+//        String s = "abc"; 
+//        System.out.println(s.charAt(3));
+//       Object o = new Object();
+//       String d = (String)o;
+//      Object o = null;
+//        System.out.println(o.toString());
+//        System.out.println(1.0/0);
+    }
+}
